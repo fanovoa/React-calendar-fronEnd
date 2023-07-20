2e2dca5129866ca2f18c0b2a2fd02a1cdbf0418d
fanovoa <fabian93novoa@gmail.com>
2023-07-19 23:08:35 -0500

    configuracion de axios

---
+++ src/api/calendarApi.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { getEnvVariables } from '../helpers';
+
+
+const { VITE_API_URL }  = getEnvVariables();
+
+const calendarApi = axios.create({
+    baseURL: VITE_API_URL
+});
+
+
+//TODO: Configurar interceptores
+
+export default calendarApi;
