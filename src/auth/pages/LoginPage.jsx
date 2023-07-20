9093249ca4786c453688836a74b03c2c8bd9d801
fanovoa <fabian93novoa@gmail.com>
2023-07-20 00:12:17 -0500

    Mostar errores en la autenticacion y crear un nuevo usuario

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import { useAuthStore, useForm } from '../../hooks';
 import './LoginPage.css';
+import Swal from 'sweetalert2';
 
 const loginFormFields = {
     loginEmail: '',
@@ -16,7 +18,7 @@ const registerFormFields = {
 
 export const LoginPage= () => {
 
-    const { startLogin } = useAuthStore();
+    const { startRegister, startLogin, errorMessage } = useAuthStore();
 
     const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm(loginFormFields);
     const { registerName, registerEmail, registerPassword, registerPassword2, onInputChange: onRegisterInputChange } = useForm(registerFormFields);
@@ -28,8 +30,22 @@ export const LoginPage= () => {
 
     const registerSubmit = ( event ) => {
         event.preventDefault();
-        console.log( { registerName, registerEmail, registerPassword, registerPassword2 } )
+        if(registerPassword !== registerPassword2){
+            Swal.fire('Error en registro', 'Contraseñas no son iguales', 'error');
+            return;
+        }
+        startRegister( { name:registerName, email:registerEmail, password: registerPassword } );
     }
+
+    useEffect(() => {
+      
+        if( errorMessage !== undefined ) {
+            Swal.fire('Error en la autenticación', errorMessage, 'error');
+        }
+    
+    
+    }, [errorMessage])
+    
 
     return (
         <div className="container login-container">
--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { calendarApi } from '../api';
-import { onChecking, onLogin, onLogout } from '../store';
+import { clearErrorMessage, onChecking, onLogin, onLogout } from '../store';
 
 export const useAuthStore = () => {
 
@@ -18,6 +18,28 @@ export const useAuthStore = () => {
 
         } catch (error) {
             dispatch( onLogout('Credenciales incorrectas'));
+            setTimeout(() => {
+                dispatch(clearErrorMessage() );
+            }, 10);
+        }
+    }
+
+    const startRegister = async({ name, email, password }) => {
+
+        dispatch( onChecking() );
+
+        try {
+            
+            const { data } = await calendarApi.post('/auth/new', {name, email, password });
+            localStorage.setItem('token',data.token);
+            localStorage.setItem('token-int-date', new Date().getTime() );
+            dispatch( onLogin({name:data.name, uid: data.uid }) );
+
+        } catch (error) {
+            dispatch( onLogout(error.response.data?.msg || '---'));
+            setTimeout(() => {
+                dispatch(clearErrorMessage() );
+            }, 10);
         }
     }
 
@@ -29,6 +51,7 @@ export const useAuthStore = () => {
 
         //* Métodos
         startLogin,
+        startRegister,
     }
 
 }
