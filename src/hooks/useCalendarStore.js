974b864c67da5ccd97b6a24a2abdde6a6faeba2d
fanovoa <fabian93novoa@gmail.com>
2023-07-24 18:09:57 -0500

    Actualizar evento

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent, onUpdateEvent } from '../store';
 import { calendarApi } from '../api';
 import { convertToDateToEvents } from '../helpers';
+import Swal from 'sweetalert2';
 
 
 export const useCalendarStore = () => {
@@ -17,17 +18,27 @@ export const useCalendarStore = () => {
 
     const startSavingEvent = async( calendarEvent ) => {
 
-      //TODO: llegar al backend
+      try {
 
-      //Todo bien
-      if(calendarEvent._id){
-            // Actualizando
-          dispatch(onUpdateEvent({ ...calendarEvent } ));
-      }else{
-        //Creando
-        const { data } = await calendarApi.post('/events', calendarEvent );
-          dispatch( onAddNewEvent({ ...calendarEvent, id: data.evento.id, user }) );
+        if(calendarEvent.id){
+          // Actualizando
+          await calendarApi.put(`/events/${calendarEvent.id}`, calendarEvent);
+          dispatch(onUpdateEvent({ ...calendarEvent, user } ));
+          return;
+        }
+      
+    
+      //Creando
+      const { data } = await calendarApi.post('/events', calendarEvent );
+      dispatch( onAddNewEvent({ ...calendarEvent, id: data.evento.id, user }) );
+        
+      } catch (error) {
+        console.log( error );
+          Swal.fire('Error al guardar', error.response.data?.msg, 'error')
       }
+
+      
+      
     }
 
     const startDeleteEvent = () =>{
